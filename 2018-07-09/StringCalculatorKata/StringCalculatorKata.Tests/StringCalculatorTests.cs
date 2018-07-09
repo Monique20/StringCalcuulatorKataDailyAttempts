996cdd21badbafb0728e4105a30b3577db6dc064
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-07-09 08:40:03 +0200

    day 9

---
+++ 2018-07-09/StringCalculatorKata/StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Add_GivenAnEmptyString_ShouldReturn0(string input)
+        {
+            //Arrange
+            var expected = 0;
+            StringCalculator sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1",1)]
+        [TestCase("2",2)]
+        [TestCase("3",3)]
+        public void Add_GivenNumberASingleNumber_ShouldReturnThatNumber(string input, int expected)
+        {
+            //Arrange
+            StringCalculator sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("1,8,9,3,5,4", 30)]
+        [TestCase("1,8,9", 18)]
+        public void Add_GivenMultipleNumbers_ShouldReturnSum(string input, int expected)
+        {
+            //Arrange
+            StringCalculator sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_GivenNewLineDelimiter_ShouldReturnSum()
+        {
+            //Arrange
+            var input = "1\n2,3";
+            var expected = 6;
+            StringCalculator sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_GivenSemiColonDelimiter_ShouldReturnSum()
+        {
+            //Arrange
+            var input = "//;\n1;2";
+            var expected = 3;
+            StringCalculator sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ 2018-07-09/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+
+            var splitNumbers = Split(numbers);
+            List<int> listOfNumbers = AddNumbersToList(splitNumbers);
+
+            return listOfNumbers.Sum();
+        }
+
+        private static List<int> AddNumbersToList(string[] splitNumbers)
+        {
+            var listOfNumbers = new List<int>();
+
+            foreach (var value in splitNumbers)
+            {
+                var number = int.Parse(value);
+                listOfNumbers.Add(number);
+            }
+
+            return listOfNumbers;
+        }
+
+        private static string[] Split(string numbers)
+        {
+            return numbers.Split(new[] { ",", "\n", ";", "/" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
