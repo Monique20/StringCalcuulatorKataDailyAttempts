514fbadc3347bee507a852a05ba31cd6c7996929
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-07-10 09:20:36 +0200

    day 10

---
+++ 2018-07-10/StringCalculatorKata/StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Add_GivenAnEmptyString_ShouldReturn0(string input)
+        {
+            //Arrange
+            var expected = 0;
+            var sut = CreateStringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1",1)]
+        [TestCase("8", 8)]
+        [TestCase("10", 10)]
+        public void Add_GivenOneNumber_ShouldReturnThatNumber(string input, int expected)
+        {
+            //Arrange
+            var sut = CreateStringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("1,2,4", 7)]
+        [TestCase("1,2,4,1", 8)]
+        public void Add_GivenMultipleNumbers_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = CreateStringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1\n2,3", 6)]
+        [TestCase("//;\n1;2", 3)]
+        public void Add_GivenDifferentDelimiters_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = CreateStringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_GivenNegativeNumber_ShouldThrowAnException()
+        {
+            //Arrange
+            var input = "-1,5,4";
+            var expected = "negatives not allowed: -1";
+            var sut = CreateStringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static StringCalculator CreateStringCalculator()
+        {
+            return new StringCalculator();
+        }
+    }
+}
---
+++ 2018-07-10/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+
+            var splitNumbers = SplitNumbers(numbers);
+            var listOfNumbers = AddToList(splitNumbers);
+
+            var negativeNumbers = numbers.Where(x => x < 0);
+            if(negativeNumbers.Any())
+            {
+                throw new Exception($"negatives not allowed {string.Join(",", negativeNumbers)}");
+            }
+
+            return listOfNumbers.Sum();
+        }
+
+        private static List<int> AddToList(string[] splitNumbers)
+        {
+            var listOfNumbers = new List<int>();
+
+            foreach (var value in splitNumbers)
+            {
+                var number = int.Parse(value);
+                listOfNumbers.Add(number);
+            }
+
+            return listOfNumbers;
+        }
+
+        private static string[] SplitNumbers(string numbers)
+        {
+            return numbers.Split(new[] { ",", "\n", ";", "/" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
