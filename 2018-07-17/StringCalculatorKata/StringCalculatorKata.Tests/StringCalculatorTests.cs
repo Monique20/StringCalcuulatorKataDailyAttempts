58322f7e624f2c64f403764415c1c817686a9731
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-07-18 08:03:36 +0200

    day 14

---
+++ 2018-07-17/StringCalculatorKata/StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Add_GivenAnEmptyStringOrNull_ShouldReturn0(string input)
+        {
+            //Arrange
+            var expected = 0;
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestCase("1",1)]
+        [TestCase("5",5)]
+        [TestCase("10",10)]
+        public void Add_GivenASingleNumber_ShouldReturnThatNumber(string input,int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestCase("1,8,1", 10)]
+        [TestCase("5,1,2,3,1", 12)]
+        [TestCase("10,1,4,5,2", 22)]
+        public void Add_GivenMultipleNumbers_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ 2018-07-17/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if(string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+
+            var splitNumbers = numbers.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+            var listOfNumbers = new List<int>();
+
+            foreach(var value in splitNumbers)
+            {
+                var number = int.Parse(value);
+                listOfNumbers.Add(number);
+            }
+
+            return listOfNumbers.Sum();
+        }
+    }
+}
