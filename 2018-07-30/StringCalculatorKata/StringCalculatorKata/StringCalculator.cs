1b45c3ba0d2ff19e59102e7c9d790f36bf3451f3
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-07-30 08:29:08 +0200

    day 17

---
+++ 2018-07-30/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+
+            var splitNumbers = SplitNumbers(numbers);
+            var listOfNumbers = AddToList(splitNumbers);
+
+            var negativeNumbers = listOfNumbers.Where(x => x < 0);
+            if(negativeNumbers.Any())
+            {
+                throw new Exception($"negatives not allowed: {string.Join(",", negativeNumbers)}");
+            }
+
+            return listOfNumbers.Sum();
+        }
+
+        private static List<int> AddToList(string[] splitNumbers)
+        {
+            var listOfNumbers = new List<int>();
+            var numb = splitNumbers.Select(x => int.Parse(x));
+            listOfNumbers.AddRange(numb);
+
+            return listOfNumbers.Where(x => x <= 1000).ToList();
+            
+        }
+
+        private static string[] SplitNumbers(string numbers)
+        {
+            return numbers.Split(new[] { ",", "\n", "/", ";", "[", "*", "%", "#", "!", "&", "]", "(", ")" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
