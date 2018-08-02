113eb70781842a05283a17fab87ce98ad2ce3e62
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-08-02 08:51:50 +0200

    day 20

---
+++ 2018-08-02/StringCalculatorKata/StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Add_GivendANullOrWhitespace_ShouldReturn0(string input)
+        {
+            //Arrange
+            var expected = 0;
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1",1)]
+        [TestCase("3",3)]
+        [TestCase("120",120)]
+        public void Add_GivendASingleNumber_ShouldReturnThatNumber(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1,4,3", 8)]
+        [TestCase("3,5,6,7", 21)]
+        [TestCase("120,50,40,60", 270)]
+        public void Add_GivenMultipleNumbers_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1,4,3", 8)]
+        [TestCase("3,5\n6,7", 21)]
+        [TestCase("120,50;40,60", 270)]
+        public void Add_GivenDifferentDelimiters_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("-1,-4,-3", "negatives not allowed: -1,-4,-3")]
+        [TestCase("-1,4,-3", "negatives not allowed: -1,-3")]
+        public void Add_GivenNegativeNumbers_ShouldThrowAnException(string input, string expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = Assert.Throws<Exception>(() => sut.Add(input));
+
+            //Assert
+            Assert.AreEqual(expected, actual.Message);
+        }
+
+        [TestCase("1, 999", 1000)]
+        [TestCase("1, 1000", 1001)]
+        [TestCase("1, 1020", 1)]
+        public void Add_GivenNumberMoreThan1000_ShouldNotAddThatNumber(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ 2018-08-02/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+
+            var splitNumbers = SplitNumbers(numbers);
+            var listOfNumbers = AddToList(splitNumbers);
+
+            CheckForNegativeNumbers(listOfNumbers);
+
+            return listOfNumbers.Sum();
+
+        }
+
+        private static void CheckForNegativeNumbers(List<int> listOfNumbers)
+        {
+            var negativeNumbers = listOfNumbers.Where(x => x < 0);
+            if (negativeNumbers.Any())
+            {
+                throw new Exception($"negatives not allowed: {string.Join(",", negativeNumbers)}");
+            }
+        }
+
+        private static List<int> AddToList(string[] splitNumbers)
+        {
+            var listOfNumbers = new List<int>();
+            var numb = splitNumbers.Select(x => int.Parse(x));
+            listOfNumbers.AddRange(numb);
+            
+            return listOfNumbers.Where(x => x <= 1000).ToList();
+        }
+
+        private static string[] SplitNumbers(string numbers)
+        {
+            return numbers.Split(new[] { ",", "\n", ";" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
