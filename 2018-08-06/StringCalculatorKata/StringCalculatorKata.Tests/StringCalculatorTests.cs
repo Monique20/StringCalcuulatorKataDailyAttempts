b1859c1ef4722b9a814f5c175d12026469471a1a
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-08-06 09:00:14 +0200

    day 20

---
+++ 2018-08-06/StringCalculatorKata/StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Add_GivenANullOrWhitespace_ShouldReturn0(string input)
+        {
+            //Arrange
+            var expected = 0;
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1",1)]
+        [TestCase("3",3)]
+        [TestCase("10", 10)]
+        public void Add_GivenASingleNumber_ShouldReturnThatNumber(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1,2,3", 6)]
+        [TestCase("3,4,5,6", 18)]
+        [TestCase("10,20,30,40", 100)]
+        public void Add_GivenMultipleNumbers_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1,2,3", 6)]
+        [TestCase("3,4\n5,6", 18)]
+        [TestCase("10,20,30;40", 100)]
+        public void Add_GivenDifferentDelimiters_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("-1,-1", "negatives not allowed: -1,-1")]
+        [TestCase("3,4,-6,4", "negatives not allowed: -6")]
+        public void Add_GivenNegativeNumbers_ShouldThrowAnException(string input, string expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = Assert.Throws<Exception>(() => sut.Add(input));
+
+            //Assert
+            Assert.AreEqual(expected, actual.Message);
+        }
+
+        [TestCase("1,999", 1000)]
+        [TestCase("3, 1000", 1003)]
+        [TestCase("10,1001", 10)]
+        public void Add_GivenANumberOver1000_ShouldNotAdd(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
